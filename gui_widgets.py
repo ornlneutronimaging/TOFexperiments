2764fbb8c8fed2b17fe51a2b1168aeba869cfba2
JeanBilheux <bilheuxjm@ornl.gov>
2016-05-02 11:13:32 -0400

    Finished up first notebook. More work needs to be done to export data

---
+++ gui_widgets.py
@@ -0,0 +1,22 @@
+from PyQt4 import QtCore, QtGui
+
+def gui_dname(dir=None):
+    """Select files"""
+    if dir is None: 
+        dir ='./'
+    if dir is "":
+        dir = './'
+        
+    dirname = QtGui.QFileDialog.getExistingDirectory(None, "Select Folder ...", 
+                                            dir, 
+                                            QtGui.QFileDialog.ShowDirsOnly)
+    return dirname
+
+def gui_fname(dir=None):
+    """Select one or more file via a dialog and returns the file name.
+    """
+    if dir is None: 
+        dir ='./'
+    fname = QtGui.QFileDialog.getOpenFileNames(None, "Select file(s)...", 
+            dir, filter="Fits files(*.fits)")
+    return fname
---
+++ normalization.py
@@ -0,0 +1,32 @@
+def calculate_average_ob_data(ob_data):
+    '''
+    Calculate the average OB
+    '''
+    sum_ob_data = ob_data[0]
+    if len(ob_data) == 1:
+        return sum_ob_data
+    else:
+        for _index in range(1, len(ob_data)):
+            _ob = ob_data[_index]
+            sum_ob_data += _ob
+        
+    mean_ob_data = sum_ob_data / len(ob_data)
+    return mean_ob_data
+
+
+def normalized_data_by_ob_file(data, mean_ob_data):
+    '''
+    return normalized data using (I-I0)/I0 where
+    I is the pixel value of the data
+    I0 is the pixel value of the open beam
+    '''
+    w.max = len(data)
+    display(w)
+    
+    normalized_data = []
+    for _index, _data in enumerate(data):
+        _normalized_data = (_data - mean_ob_data) / (mean_ob_data)
+        normalized_data.append(_normalized_data)
+        w.value = _index
+        
+    return normalized_data
