5b2cb7caaec1ef45367066740232dfc32007fd25
JeanBilheux <bilheuxjm@ornl.gov>
2016-05-05 09:05:18 -0400

    Fixed code when spectra file is misisng

--- gui_widgets.py
+++ gui_widgets.py
@@ -21,6 +21,15 @@ def gui_fname(dir=None):
             dir, filter="Fits files(*.fits)")
     return fname
 
+def gui_single_file(dir=None):
+    """Select one o file via a dialog and returns the file name.
+    """
+    if dir is None: 
+        dir ='./'
+    fname = QtGui.QFileDialog.getOpenFileName(None, "Select file...", 
+            dir, filter="Spectra File (*_Spectra.txt)")
+    return fname
+
 def gui_csv_fname(dir=None):
     """Select one or more file via a dialog and returns the file name.
     """
